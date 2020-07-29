665c0caddeda3301b995efcdbdeba95183f71777
Jenise Ruehle <jela07@gmail.com>
2020-07-29 22:29:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,18 +1,18 @@
 INSERT INTO users (id, name, age) VALUES (1, "Mulan", 28), (2, "Belle", 30), (3, "Tiana", 31), (4, "Pocahontas", 30), (5, "Merida", 26), (6, "Rapunzel", 29), (7, "Jasmine", 32), (8, "Elsa", 35), (9, "Anna", 33), (10, "Ariel", 32), (11, "Jane", 34), (12, "Moana", 28), (13, "Shuri", 28), (14, "Sally", 33), (15, "Kida", 35), (16, "Aurora", 40), (17, "Snow White", 52), (18, "Thumbelina", 45), (19, "Cinderella", 43), (20, "Tinkerbell", 150); 
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
-(1, "book drive", "books", 500, 2012, 2020),
-(2, "concert", "music", 10000, 2018, 2019),
-(3, "food drive", "food", 2500, 2018, 2020),
-(4, "chocolate", "food", 250, 2020, 2020),
-(5, "social justice", "change", 100000, 2019, 2025),
-(6, "beading class", "jewelry", 1000, 2020, 2020),
-(7, "virtual tour", "travel", 2500, 2019, 2022),
-(8, "astral travel guide", "travel", 150, 2020, 2020),
-(9, "virtual candle maker", "misc", 80, 2020, 2020),
-(10, "virtual cooking class", "food", 150, 2020, 2021);
+(1, "book drive", "books", 500.00, 2012, 2020),
+(2, "concert", "music", 10000.00, 2018, 2019),
+(3, "food drive", "food", 2500.00, 2018, 2020),
+(4, "chocolate", "food", 250.00, 2020, 2020),
+(5, "social justice", "change", 100000.00, 2019, 2025),
+(6, "beading class", "jewelry", 1000.00, 2020, 2020),
+(7, "virtual tour", "travel", 2500.00, 2019, 2022),
+(8, "astral travel guide", "travel", 150.00, 2020, 2020),
+(9, "virtual candle maker", "misc", 80.00, 2020, 2020),
+(10, "virtual cooking class", "food", 150.00, 2020, 2021);
 
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
-(1, 10.00, 1, 2),
+(1, 5.00, 1, 2),
 (2, 20.00, 1, 3),
 (3, 40.00, 1, 4),
 (4, 50.00, 2, 3),
