7ad26edca756d25b20f7227f46ea73154c080bc8
Jenise Ruehle <jela07@gmail.com>
2020-07-29 22:28:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,13 @@
-INSERT INTO users (id, name, age) VALUES (1, "Mulan", 28), (2, "Belle", 30), (3, "Tiana", 31), (4, "Pocahontas", 30), (5, "Merida", 26), (6, "Rapunzel", 29), (7, "Jasmine", 32), (8, "Elsa", 35), (9, "Anna", 33), (10, "Ariel", 32), (11, "Jane", 34), (12, "Moana", 28), (13, "Shuri", 28), (14, "Sally", 33), (15, "Kida", 35), (16, "Aurora", 40), (17, "Snow White", 52), (18, "Thumbelina", 45), (19, "Cinderella", 43), (20, "Tinkerbell", 150) 
+INSERT INTO users (id, name, age) VALUES (1, "Mulan", 28), (2, "Belle", 30), (3, "Tiana", 31), (4, "Pocahontas", 30), (5, "Merida", 26), (6, "Rapunzel", 29), (7, "Jasmine", 32), (8, "Elsa", 35), (9, "Anna", 33), (10, "Ariel", 32), (11, "Jane", 34), (12, "Moana", 28), (13, "Shuri", 28), (14, "Sally", 33), (15, "Kida", 35), (16, "Aurora", 40), (17, "Snow White", 52), (18, "Thumbelina", 45), (19, "Cinderella", 43), (20, "Tinkerbell", 150); 
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
-(1, "book drive", "books", 500, 2012, 2020)
-(2, "concert", "music", 10000, 2018, 2019)
-(3, "food drive", "food", 2500, 2018, 2020)
-(4, "chocolate", "food", 250, 2020, 2020)
-(5, "social justice", "change", 100000, 2019, 2025)
-(6, "beading class", "jewelry", 1000, 2020, 2020)
-(7, "virtual tour", "travel", 2500, 2019, 2022)
-(8, "astral travel guide", "travel", 150, 2020, 2020)
-(9, "virtual candle maker", "misc", 80, 2020, 2020)
-(10, "virtual cooking class", "food", 150, 2020, 2021)
+(1, "book drive", "books", 500, 2012, 2020),
+(2, "concert", "music", 10000, 2018, 2019),
+(3, "food drive", "food", 2500, 2018, 2020),
+(4, "chocolate", "food", 250, 2020, 2020),
+(5, "social justice", "change", 100000, 2019, 2025),
+(6, "beading class", "jewelry", 1000, 2020, 2020),
+(7, "virtual tour", "travel", 2500, 2019, 2022),
+(8, "astral travel guide", "travel", 150, 2020, 2020),
+(9, "virtual candle maker", "misc", 80, 2020, 2020),
+(10, "virtual cooking class", "food", 150, 2020, 2021);
 
