3c9369dcc5c9132b1702d50aa9811bc5c0280a56
Jenise Ruehle <jela07@gmail.com>
2020-07-29 22:18:07 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO users (id, name, age) VALUES (1, "Mulan", 28),
+INSERT INTO users (id, name, age) VALUES (1, "Mulan", 28), (2, "Belle", 30), (3, "Tiana", 31), (4, "Pocahontas", 30), (5, "Merida", 26), (6, "Rapunzel", 29), (7, "Jasmine", 32), (8, "Elsa", 35), (9, "Anna", 33), 
